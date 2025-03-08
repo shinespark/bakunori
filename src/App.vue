ef106f466fae6ea6816942d874f2bec7c6beb21f
shinespark <shnsprk@gmail.com>
2025-03-08 14:42:28 +0900

    ロゴ設置

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <v-app>
     <v-main>
+      <v-img src="@/assets/logo.png" alt="logo" width="100" height="100" />
       <router-view />
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts" setup>
-  //
-</script>
+<script lang="ts" setup></script>
